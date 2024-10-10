9786b5207a1bea7f1b68ad7c07d3d9c7bbd26641
Rakib <hellorakibhossen@gmail.com>
2024-10-10 06:35:07 -0700

    addede complete project section functionalitis

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const submitbtn = () => {
         <div class="modal-action">
           <form method="dialog">
             <!-- if there is a button in form, it will close the modal -->
-            <button class="bg-lime-400 py-3 w-96 rounded-md">confirm</button>
+            <button class="bg-lime-400 py-3 w-72 rounded-md">confirm</button>
           </form>
         </div>
       </div>
@@ -42,4 +42,20 @@ phone.addEventListener("input", function (e) {
   if (number.length > 10) {
     btn.removeAttribute("disabled");
   }
+});
+
+let valuesDisplays = document.querySelectorAll(".num");
+let interval = 3000;
+
+valuesDisplays.forEach((valuesDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valuesDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valuesDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
 });
